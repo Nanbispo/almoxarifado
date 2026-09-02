76cf456395770bb13a05f44d696146b23524f11c
Renan Bispo <renanjesusbispo@gmail.com>
2026-09-02 16:18:51 -0300

    estabelecendo a conexao com o banco de dados

--- config/connection.go
+++ config/connection.go
@@ -1,5 +1,30 @@
-package config 
+package config
 
-func ConnectionDB (sql.DB, error){
-	
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	cfg, err := LoadConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DB_HOST, cfg.DB_PORT, cfg.DB_USER, cfg.DB_PASSWORD, cfg.DB_NAME)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao abrir manipulador do banco: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("erro ao conectar ao banco de dados: %w", err)
+	}
+
+	return db, nil
 }
